411233256514deea219e6e500a37bd2211582c50
peter20051029-sketch <168543357+Peter292005@users.noreply.github.com>
2026-03-20 00:55:51 -0400

    feat: implementar locks compartidos y exclusivos para acceso concurrente

--- Proyecto2SO/src/Main/Main.java
+++ Proyecto2SO/src/Main/Main.java
@@ -8,42 +8,44 @@ package Main;
  *
  * @author Peter
  */
-
-import Estructuras.ListaEnlazada;
+import Concurrencia.GestorLocks;
 import Procesos.Proceso;
 import Procesos.SolicitudIO;
 import Procesos.TipoOperacionIO;
-import Scheduler.PlanificadorCSCAN;
-import Scheduler.PlanificadorFIFO;
-import Scheduler.PlanificadorSCAN;
-import Scheduler.PlanificadorSSTF;
 
 public class Main {
     public static void main(String[] args) {
-        ListaEnlazada<SolicitudIO> solicitudes = new ListaEnlazada<>();
+        GestorLocks gestor = new GestorLocks();
 
         Proceso p1 = new Proceso(1, "P1");
         Proceso p2 = new Proceso(2, "P2");
+        Proceso p3 = new Proceso(3, "P3");
 
-        solicitudes.agregar(new SolicitudIO(1, p1, TipoOperacionIO.READ, "a.txt", 95));
-        solicitudes.agregar(new SolicitudIO(2, p2, TipoOperacionIO.WRITE, "b.txt", 180));
-        solicitudes.agregar(new SolicitudIO(3, p1, TipoOperacionIO.READ, "c.txt", 34));
-        solicitudes.agregar(new SolicitudIO(4, p2, TipoOperacionIO.DELETE, "d.txt", 119));
-        solicitudes.agregar(new SolicitudIO(5, p1, TipoOperacionIO.CREATE, "e.txt", 11));
-        solicitudes.agregar(new SolicitudIO(6, p2, TipoOperacionIO.READ, "f.txt", 123));
-        solicitudes.agregar(new SolicitudIO(7, p1, TipoOperacionIO.WRITE, "g.txt", 62));
-        solicitudes.agregar(new SolicitudIO(8, p2, TipoOperacionIO.READ, "h.txt", 64));
+        SolicitudIO s1 = new SolicitudIO(1, p1, TipoOperacionIO.READ, "archivo.txt", 10);
+        SolicitudIO s2 = new SolicitudIO(2, p2, TipoOperacionIO.READ, "archivo.txt", 10);
+        SolicitudIO s3 = new SolicitudIO(3, p3, TipoOperacionIO.WRITE, "archivo.txt", 10);
 
-        System.out.println("FIFO:");
-        System.out.println(new PlanificadorFIFO(50).planificar(solicitudes));
+        System.out.println("P1 solicita lectura: " + gestor.solicitarLock(s1));
+        System.out.println("P2 solicita lectura: " + gestor.solicitarLock(s2));
+        System.out.println("P3 solicita escritura: " + gestor.solicitarLock(s3));
 
-        System.out.println("SSTF:");
-        System.out.println(new PlanificadorSSTF(50).planificar(solicitudes));
+        System.out.println("Estado locks:");
+        System.out.println(gestor);
 
-        System.out.println("SCAN:");
-        System.out.println(new PlanificadorSCAN(50, true).planificar(solicitudes));
+        System.out.println("Estados procesos:");
+        System.out.println(p1);
+        System.out.println(p2);
+        System.out.println(p3);
 
-        System.out.println("C-SCAN:");
-        System.out.println(new PlanificadorCSCAN(50, true).planificar(solicitudes));
+        gestor.liberarLock(s1);
+        System.out.println("Después de liberar lectura de P1:");
+        System.out.println(gestor);
+
+        gestor.liberarLock(s2);
+        System.out.println("Después de liberar lectura de P2:");
+        System.out.println(gestor);
+
+        System.out.println("Estado final de P3:");
+        System.out.println(p3);
     }
 }
